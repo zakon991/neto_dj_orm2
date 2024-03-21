373f7280df94335e951e8a55145d3fa9545ab4c3
zakon <zakon991@gmail.com>
2024-03-21 20:18:57 +0300

    initial commit

---
+++ school/views.py
@@ -0,0 +1,18 @@
+from django.views.generic import ListView
+from django.shortcuts import render
+
+from .models import Student
+
+
+def students_list(request):
+    template = 'school/students_list.html'
+    students = Student.objects.all().order_by('group')
+    context = {
+        "object_list": students,
+    }
+
+    # используйте этот параметр для упорядочивания результатов
+    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
+    # ordering = 'group'
+
+    return render(request, template, context)
